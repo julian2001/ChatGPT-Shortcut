2afa1a93d3c852e37643e2a4980e8c9d24da56d7
julian2001 <julianr2000@yahoo.com>
2023-09-14 15:33:11 -0500

    Update Package.swift
    Version 0.1 of the Reyes-Spinozaapp SiriKit Package

--- Package.swift
+++ Package.swift
@@ -1,21 +1,34 @@
 
 
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
-	name: "ChatGPT-Shortcut",
-	products: [
-		.library(
-			name: "ChatGPT-Shortcut",
-			targets: ["ChatGPT-Shortcut"]),
-	],
-	targets: [
-		.target(
-			name: "ChatGPT-Shortcut",
-			dependencies: [],
-			path: "Sources"),
-		.testTarget(
-			name: "ChatGPT-ShortcutTests",
-			dependencies: ["ChatGPT-Shortcut"]),
-	])
+    name: "ChatGPT-Shortcut",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "ChatGPT-Shortcut",
+            targets: ["ChatGPT-Shortcut"]
+        ),
+    ],
+    dependencies: [
+        // Add your package dependencies here, for example:
+        // .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "ChatGPT-Shortcut",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ChatGPT-ShortcutTests",
+            dependencies: ["ChatGPT-Shortcut"]
+        ),
+    ]
+)
